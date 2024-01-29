e97bfc4fff89168d013658c118341375aecef8c8
Venkata Sreenivas Prasad Kasibhatla <vn769140@dal.ca>
2024-01-29 12:29:56 -0400

    adding a git repo

---
+++ Tutorial2/my-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>It works</h1>
+        <p>Venkata Sreenivas Prasad - B00972626</p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
